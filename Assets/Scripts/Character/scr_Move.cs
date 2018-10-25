61db0a2570af374f2d63a436f2fe1b3778b75fc8
Stanislas <s.dubuc@rubika-edu.com>
2018-10-25 15:02:29 +0200

    Test Chara

--- Assets/Scripts/Character/scr_Move.cs
+++ Assets/Scripts/Character/scr_Move.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_Move : MonoBehaviour {
+public class scr_Move : MonoBehaviour
+{
+	public GameObject touchEffect;
 
-	// Use this for initialization
-	void Start () {
-		
+	Touch touch;
+	Ray ray;
+	RaycastHit hit;
+
+	private void Update()
+	{
+		GetInput();
+		Move();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Move()
+	{
+		this.transform.position = Vector3.Lerp(this.transform.position, hit.point, Time.deltaTime);
+	}
+
+	void GetInput()
+	{
+		if (Input.touchCount > 0)
+		{
+			touch = Input.GetTouch(0);
+			ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			if (Physics.Raycast(ray, out hit))
+			{
+				Instantiate(touchEffect, hit.point, new Quaternion(0, 0, 0, 0));
+			}
+		}
 	}
 }
